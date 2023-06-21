1b78a99fb36564a4d85e04c2d984c6c3c3261a19
nathandecaux <nathan.decaux@hotmail.fr>
2023-06-21 08:58:21 +0200

    Updating test scripts

--- src/napari_labelprop/_tests/test_widget.py
+++ src/napari_labelprop/_tests/test_widget.py
@@ -3,31 +3,15 @@ import numpy as np
 
 # make_napari_viewer is a pytest fixture that returns a napari viewer object
 # capsys is a pytest fixture that captures stdout and stderr output streams
-def test_example_q_widget(make_napari_viewer, capsys):
-    # make viewer and add an image layer using our fixture
-    viewer = make_napari_viewer()
-    viewer.add_image(np.random.random((100, 100)))
-
-    # create our widget, passing in the viewer
-    my_widget = ExampleQWidget(viewer)
-
-    # call our widget method
-    my_widget._on_click()
-
-    # read captured output and check that it's as we expected
-    captured = capsys.readouterr()
-    assert captured.out == "napari has 1 layers\n"
     
 def test_example_magic_widget(make_napari_viewer, capsys):
     viewer = make_napari_viewer()
     layer = viewer.add_image(np.random.random((100, 100)))
 
     # this time, our widget will be a MagicFactory or FunctionGui instance
-    my_widget = example_magic_widget()
-
-    # if we "call" this object, it'll execute our function
-    my_widget(viewer.layers[0])
+    dw, my_widget = viewer.window.add_plugin_dock_widget('napari-labelprop', 'Training')
+    dw2, my_widget2 = viewer.window.add_plugin_dock_widget('napari-labelprop', 'Inference')
+    
 
     # read captured output and check that it's as we expected
     captured = capsys.readouterr()
-    assert captured.out == f"you have selected {layer}\n"
